7b03b9168fe072929e1047ad7913189cae3d5591
Rafath Bin Zafar Auvee <rbza98@outlook.com>
2021-04-23 20:21:38 +0600

    Buttons are working"

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,7 +2,7 @@ export const Data = [
   {
     name: "Project-1",
     image:
-      "https://images.unsplash.com/photo-1607034071833-18a5b64047ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+      "https://images.unsplash.com/photo-1562408590-e32931084e23?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
   },
   {
     name: "Project-2",
@@ -17,6 +17,6 @@ export const Data = [
   {
     name: "Project-4",
     image:
-      "https://images.unsplash.com/photo-1562408590-e32931084e23?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      "https://images.unsplash.com/photo-1607034071833-18a5b64047ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
   },
 ];
--- src/components/Slide.js
+++ src/components/Slide.js
@@ -3,8 +3,16 @@ import { Data } from "./Data.js";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
 const Slide = ({ slides }) => {
-  const [current, setCurrnet] = useState(0);
+  const [current, setCurrent] = useState(0);
   const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
 
   if (!Array.isArray(slides) || slides.length <= 0) {
     return null;
@@ -12,10 +20,19 @@ const Slide = ({ slides }) => {
 
   return (
     <section className="slider">
-      <FaArrowAltCircleLeft className="left-arrow" />
-      <FaArrowAltCircleRight className="right-arrow" />
+      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
       {Data.map((slider, index) => {
-        return <img src={slider.image} alt="project x" className="image" />;
+        return (
+          <div
+            className={index === current ? "slide active" : "slide"}
+            key={index}
+          >
+            {index === current && (
+              <img src={slider.image} alt="project x" className="image" />
+            )}
+          </div>
+        );
       })}
     </section>
   );
